1f91fc80bfb97393f6c932c24accbd6921f4e20a
Afjal13 <afjal.hossain12496@gmail.com>
2023-09-30 23:40:18 +0600

    update 09-30-2023

---
+++ Calculator_Application/Calculator_Application/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Calculator
+{
+    public int Value1 { get; set; }
+    public int Value2 { get; set; }
+    public int Value3 { get; set; }
+    public int Result { get; set; }
+
+    public Calculator()
+    {
+        Result = 0;
+
+    }
+    //  public Calculator(int Value1, int Value2)
+    //  {
+    //      this.Value1 = Value1;
+    //      this.Value2 = Value2;
+    //      Result = 0;
+    //  }
+    //  public Calculator(int Value1, int Value2, int Value3)
+    //  {
+    //this.Value1 = Value1; 
+    //this.Value2 = Value2; 
+    //this.Value3 = Value3;
+    //      Result = 0;
+
+    //  }
+
+    public int Add(int Value1, int Value2)
+    {
+        Result = Value1 + Value2;
+
+        return Result;
+    }
+
+}
--- Calculator_Application/Calculator_Application/Program.cs
+++ Calculator_Application/Calculator_Application/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator obj = new Calculator();
+        Console.WriteLine(obj.Add(2, 3));
+        Console.ReadKey();
+        
+    }
+}
