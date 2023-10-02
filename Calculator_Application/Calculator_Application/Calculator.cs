ce29a9fdd9cde10dc5a7e398f740925014b0eb06
Afjal13 <afjal.hossain12496@gmail.com>
2023-10-02 23:17:19 +0600

    Updated 10-02-2023

--- Calculator_Application/Calculator_Application/Calculator.cs
+++ Calculator_Application/Calculator_Application/Calculator.cs
@@ -44,4 +44,55 @@ public class Calculator
         Result = value1 + value2 + value3;
         return Result;
     }
+
+    public int Substract()
+    {
+        Result = Value1 - Value2 - Value3;
+        return Result;
+    }
+
+    public int Substract(int value1, int value2)
+    {
+        Result = value1 - value2;
+        return Result;
+    }
+
+    public int Substract(int value1, int value2, int value3)
+    {
+        Result = value1 - value2 - value3;
+        return Result;
+    }
+    public int Div()
+    {
+        Result = (int)Value1 / Value2;
+        return Result;
+    }
+
+    public int Div(int value1, int value2)
+    {
+        Result = (int)(value1 / value2);
+        return Result;
+    }
+
+    public int Multiply()
+    {
+        if(Value3==0)
+            Result = Value1 * Value2;
+        else
+            Result = Value1 * Value2 * Value3;
+        return Result;
+    }
+
+    public int Multiply(int value1, int value2)
+    {
+        Result = value1 * value2;
+        return Result;
+    }
+
+    public int Multiply(int value1, int value2, int value3)
+    {
+        Result = value1 * value2 * value3;
+        return Result;
+    }
+
 }
--- Calculator_Application/Calculator_Application/Program.cs
+++ Calculator_Application/Calculator_Application/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main()
     {
-        Calculator objCallByConstructor = new Calculator(2,5,3);
-        Console.WriteLine($"\nCalculate Value by Constructor Result is:{objCallByConstructor.Add()}");
+        Calculator objCallByConstructor = new Calculator(12,3);
+        Console.WriteLine($"\nCalculate Add Value by Constructor Result is:{objCallByConstructor.Add()}");
+        Console.WriteLine($"\nCalculate Substract Value by Constructor Result is:{objCallByConstructor.Substract()}");
+        Console.WriteLine($"\nCalculate Div Value by Constructor Result is:{objCallByConstructor.Div()}");
+        Console.WriteLine($"\nCalculate Multiply Value by Constructor Result is:{objCallByConstructor.Multiply()}");
         
         Calculator obj = new Calculator();
-        Console.WriteLine($"\nCalculate Value by Method Result is:{obj.Add(2,5)}");
+        Console.WriteLine($"\nCalculate Add Value by Method Result is:{obj.Add(2,5)}");
         
         Console.ReadKey();
         
